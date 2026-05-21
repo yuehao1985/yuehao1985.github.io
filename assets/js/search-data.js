4cbf03b81463963989ccd680f82f75ea4a934623
yuehao1985 <74018143+yuehao1985@users.noreply.github.com>
2026-05-21 03:58:13 +0000

    Deploying to gh-pages from @ yuehao1985/yuehao1985.github.io@93a4bcbd23455e0c80100b40dba61eb2e2937d91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "Course materials, schedules, and resources for classes taught.",
           section: "Navigation",
           handler: () => {
