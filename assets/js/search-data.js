14f7c7c559e6d35d065991e9cdbbdb9c8c4a4b26
yuehao1985 <74018143+yuehao1985@users.noreply.github.com>
2026-05-21 05:10:37 +0000

    Deploying to gh-pages from @ yuehao1985/yuehao1985.github.io@3bddce963727abfb42da70c93e568ac936ed5d5a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "Course materials, schedules, and resources for classes taught.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
